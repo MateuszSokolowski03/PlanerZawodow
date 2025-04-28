43b1b63ea531b58512d4eb9a1460a52f4e29a7ff
kacperDebowski <89303@student.pb.edu.pl>
2025-04-28 14:59:07 +0200

    widoki i template'y

--- PlanerZawodow/urls.py
+++ PlanerZawodow/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from zawodowplaner import views
+from zawodowplaner.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', HomePageView.as_view(), name='home'),
+
+    path('zawody/', views.ZawodyListView.as_view(), name='zawody-list'),
+    path('zawody/<int:pk>/', views.ZawodyDetailView.as_view(), name='zawody-detail'),
+    path('zawody/dodaj/', views.ZawodyCreateView.as_view(), name='zawody-create'),
+
+    path('kolejka/dodaj/', views.KolejkaCreateView.as_view(), name='kolejka-create'),
+
+    path('druzyny/', views.DruzynaListView.as_view(), name='druzyna-list'),
+
+    path('powiadomienia/', views.PowiadomienieListView.as_view(), name='powiadomienie-list'),
 ]
--- zawodowplaner/views.py
+++ zawodowplaner/views.py
@@ -1,3 +1,129 @@
-from django.shortcuts import render
+# views.py
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import (
+    zawody, kolejka, druzyna, zgloszenie,
+    zawodnik, mecz, wydarzenie, powiadomienie
+)
 
-# Create your views here.
+
+# Widok dla strony poczatkowej
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+# Widoki dla Zawodów
+class ZawodyListView(ListView):
+    model = zawody
+    template_name = 'zawody/list.html'
+    context_object_name = 'zawody'
+    paginate_by = 10
+
+
+class ZawodyDetailView(DetailView):
+    model = zawody
+    template_name = 'zawody/detail.html'
+
+
+class ZawodyCreateView(LoginRequiredMixin, CreateView):
+    model = zawody
+    template_name = 'zawody/form.html'
+    fields = ['nazwa', 'id_organizatora', 'data_rozpoczecia',
+              'data_zakonczenia', 'opis', 'maks_zespolow', 'regulaminy']
+    success_url = reverse_lazy('zawody-list')
+
+
+class ZawodyUpdateView(LoginRequiredMixin, UpdateView):
+    model = zawody
+    template_name = 'zawody/form.html'
+    fields = ['nazwa', 'data_rozpoczecia', 'data_zakonczenia',
+              'opis', 'status', 'maks_zespolow', 'regulaminy']
+    success_url = reverse_lazy('zawody-list')
+
+
+class ZawodyDeleteView(LoginRequiredMixin, DeleteView):
+    model = zawody
+    template_name = 'zawody/confirm_delete.html'
+    success_url = reverse_lazy('zawody-list')
+
+
+# Widoki dla Kolejek
+class KolejkaCreateView(LoginRequiredMixin, CreateView):
+    model = kolejka
+    template_name = 'kolejka/form.html'
+    fields = ['id_zawodu', 'numer', 'nazwa', 'data_rozpoczecia', 'data_zakonczenia']
+    success_url = reverse_lazy('zawody-list')
+
+
+class KolejkaUpdateView(LoginRequiredMixin, UpdateView):
+    model = kolejka
+    template_name = 'kolejka/form.html'
+    fields = ['numer', 'nazwa', 'data_rozpoczecia', 'data_zakonczenia', 'czy_zakonczona']
+    success_url = reverse_lazy('zawody-list')
+
+
+# Widoki dla Drużyn
+class DruzynaListView(ListView):
+    model = druzyna
+    template_name = 'druzyna/list.html'
+    context_object_name = 'druzyny'
+
+
+class DruzynaCreateView(LoginRequiredMixin, CreateView):
+    model = druzyna
+    template_name = 'druzyna/form.html'
+    fields = ['id_kapitana', 'nazwa', 'herb_url']
+    success_url = reverse_lazy('druzyna-list')
+
+
+# Widoki dla Zgłoszeń
+class ZgloszenieCreateView(LoginRequiredMixin, CreateView):
+    model = zgloszenie
+    template_name = 'zgloszenie/form.html'
+    fields = ['id_zawodu', 'id_druzyny']
+    success_url = reverse_lazy('zgloszenie-list')
+
+    def form_valid(self, form):
+        form.instance.status = 'oczekujaca'
+        return super().form_valid(form)
+
+
+# Widoki dla Zawodników
+class ZawodnikCreateView(LoginRequiredMixin, CreateView):
+    model = zawodnik
+    template_name = 'zawodnik/form.html'
+    fields = ['id_druzyny', 'imie', 'nazwisko', 'data_urodzenia',
+              'pozycja', 'numer_koszulki', 'zdjecie_url']
+    success_url = reverse_lazy('druzyna-list')
+
+
+# Widoki dla Meczy
+class MeczDetailView(DetailView):
+    model = mecz
+    template_name = 'mecz/detail.html'
+
+
+class MeczCreateView(LoginRequiredMixin, CreateView):
+    model = mecz
+    template_name = 'mecz/form.html'
+    fields = ['id_kolejki', 'id_zawodu', 'data_meczu', 'miejsce',
+              'druzyna_gospodarz', 'druzyna_gosc', 'sedzia_glowny']
+    success_url = reverse_lazy('mecz-list')
+
+
+# Widoki dla Powiadomień
+class PowiadomienieListView(LoginRequiredMixin, ListView):
+    model = powiadomienie
+    template_name = 'powiadomienie/list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(id_uzytkownika=self.request.user)
+
+
+class PowiadomienieUpdateView(LoginRequiredMixin, UpdateView):
+    model = powiadomienie
+    template_name = 'powiadomienie/form.html'
+    fields = ['przeczytane']
+    success_url = reverse_lazy('powiadomienie-list')
