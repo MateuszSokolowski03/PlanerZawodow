adbeba08f3420d20c97a150f50873023cdff7151
MateuszSokolowski03 <eusmateusz@gmail.com>
2025-04-28 22:24:54 +0200

    Dodanie ograniczeń organizatorowi

--- zawodowplaner/models/user_models.py
+++ zawodowplaner/models/user_models.py
@@ -29,8 +29,14 @@ class uzytkownik(models.Model):
 class organizator(models.Model):
     id_organizatora = models.AutoField(primary_key=True)
     id_uzytkownika = models.ForeignKey(uzytkownik, on_delete=models.CASCADE)
-    imie = models.CharField(max_length=50)
-    nazwisko = models.CharField(max_length=50)
+    imie = models.CharField(
+        max_length=50,
+        validators=[RegexValidator(regex=r'^[^\d]*$', message="Imię nie może zawierać cyfr.")],
+    )
+    nazwisko = models.CharField(
+        max_length=50,
+        validators=[RegexValidator(regex=r'^[^\d]*$', message="Nazwisko nie może zawierać cyfr.")],
+    )
     telefon = models.CharField(
         max_length = 9,
         validators=[RegexValidator(regex=r'^\d{9}$', message="Numer telefonu musi mieć 9 cyfr.")],
@@ -45,6 +51,10 @@ class organizator(models.Model):
         # Sprawdź, czy powiązany użytkownik ma typ "organizator"
         if self.id_uzytkownika.typ_uzytkownika != 'organizator':
             raise ValidationError("Tylko użytkownik o typie 'organizator' może być przypisany jako organizator.")
+         # Sprawdź, czy istnieje już organizator z tym samym użytkownikiem
+        if organizator.objects.filter(id_uzytkownika=self.id_uzytkownika).exists():
+            raise ValidationError("Organizator z tym adresem e-mail już istnieje.")       
+
     def __str__(self):
         return f"{self.imie} {self.nazwisko} ({self.telefon})"
     class Meta:
