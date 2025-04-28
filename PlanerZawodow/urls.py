dca10caab3930db6a4affd0eecdf8e326279dd42
MateuszSokolowski03 <eusmateusz@gmail.com>
2025-04-28 15:32:24 +0200

    Poprawka urls

--- PlanerZawodow/urls.py
+++ PlanerZawodow/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for PlanerZawodow project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from zawodowplaner import views
@@ -27,7 +11,7 @@ urlpatterns = [
     path('zawody/', views.ZawodyListView.as_view(), name='zawody-list'),
     path('zawody/<int:pk>/', views.ZawodyDetailView.as_view(), name='zawody-detail'),
     path('zawody/dodaj/', views.ZawodyCreateView.as_view(), name='zawody-create'),
-    path('zawody/update/', views.ZawodyUpdateView.as_view(), name='zawody-update'),
+    path('zawody/<int:pk>/update/', views.ZawodyUpdateView.as_view(), name='zawody-update'),
 
     path('kolejka/dodaj/', views.KolejkaCreateView.as_view(), name='kolejka-create'),
 
